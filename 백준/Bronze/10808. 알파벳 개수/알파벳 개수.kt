a7abcd27fc0d8bc4da8524826216d4b8a0646f5b
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-01-09 21:17:57 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 88 ms, Memory: 12188 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.kt
@@ -0,0 +1,13 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val s = readLine()
+    val arr = IntArray(26)
+    for (i in s) {
+        arr[i -'a']++
+    }
+    for (i in arr){
+        print("$i ")
+    }
+}
