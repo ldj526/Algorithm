670d946163fba669e7d9f2b56686e5751892e203
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-01-12 16:29:43 +0900

    [Silver V] Title: 방 번호, Time: 88 ms, Memory: 12136 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.kt
+++ 백준/Silver/1475. 방 번호/방 번호.kt
@@ -2,33 +2,23 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
-    val num = readLine()
-    val number = IntArray(10)
-    val number69 = IntArray(2)
-    for (i in num) {
-        when (i) {
-            '6' -> {
-                number69[0]++
-            }
-            '9' -> {
-                number69[1]++
-            }
-            else -> {
-                number[i - '0']++
+    val n = readLine()
+    val arr = IntArray(10)
+    for (i in n) {
+        arr[i - '0']++
+    }
+    var tmp = 0
+    for (i in 0..9) {
+        if (i != 6 && i != 9) {
+            if (arr[i] > tmp) {
+                tmp = arr[i]
             }
         }
     }
-    var sum = number69[0] + number69[1]
-    var a = 0
-    if (sum % 2 == 0) {
-        a = sum / 2
+    val cnt = (arr[6] + arr[9] + 1) / 2
+    if (tmp >= cnt) {
+        println(tmp)
     } else {
-        a = sum / 2 + 1
-    }
-    number.sort()
-    if (a > number[9]) {
-        println(a)
-    } else {
-        println(number[9])
+        println(cnt)
     }
 }
