8d6c9c8e7270d8ef244938ccd72899fa54098792
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-05-04 18:25:53 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1952 ms, Memory: 87800 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.kt
@@ -0,0 +1,16 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = readln().toInt()
+    val arr = arrayListOf<Pair<Int, Int>>()
+    for (i in 0 until n) {
+        val str = br.readLine().split(" ")
+        arr.add(Pair(str[0].toInt(), str[1].toInt()))
+    }
+    val sortedArr = arr.sortedWith(compareBy({ it.first }, { it.second }))
+    for (i in 0 until n) {
+        println("${sortedArr[i].first} ${sortedArr[i].second}")
+    }
+}
