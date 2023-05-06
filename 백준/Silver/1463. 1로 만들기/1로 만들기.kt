054386fc0a60134b99da4c5bf43a11271b2fe563
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-05-06 18:43:20 +0900

    [Silver III] Title: 1로 만들기, Time: 128 ms, Memory: 16952 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.kt
@@ -0,0 +1,21 @@
+import kotlin.math.min
+
+fun main() {
+    val input = getInput()
+    println(dpSolution(input))
+}
+
+fun getInput(): Int {
+    return readln().toInt()
+}
+
+fun dpSolution(input: Int): Int {
+    val dp = IntArray(1_000_002) { 0 }
+    dp[1] = 0
+    for (i in 2..input) {
+        dp[i] = dp[i - 1] + 1
+        if (i % 2 == 0) dp[i] = min(dp[i], dp[i / 2] + 1)
+        if (i % 3 == 0) dp[i] = min(dp[i], dp[i / 3] + 1)
+    }
+    return dp[input]
+}
