d852fb8805e31fd87f5f1d3ac7d62d66bf652058
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-05-10 00:23:07 +0900

    [Bronze I] Title: 소인수분해, Time: 88 ms, Memory: 12380 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.kt
@@ -0,0 +1,12 @@
+import kotlin.math.sqrt
+
+fun main() {
+    var n = readln().toInt()
+    for (i in 2..sqrt(n.toDouble()).toInt()) {
+        while (n % i == 0) {
+            println(i)
+            n /= i
+        }
+    }
+    if (n != 1) println(n)
+}
