4ae25a80119af70e8821d85a7c65566ebd71fcb1
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-05-25 18:13:01 +0900

    [unrated] Title: 둘만의 암호, Time: 10.08 ms, Memory: 61.4 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.kt
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.kt
@@ -1,26 +1,11 @@
 class Solution {
     fun solution(s: String, skip: String, index: Int): String {
         var answer: String = ""
+        val ch = ('a' .. 'z').filter { it !in skip }
         
         s.forEach {
-            var ch = it
-            repeat(index) {
-                if(ch == 'z') {
-                    ch -= 25
-                } else {
-                    ch++
-                }
-                if(skip.contains(ch)) {
-                    while(skip.contains(ch)) {
-                        if(ch == 'z') {
-                            ch -= 25
-                        } else {
-                            ch++
-                        }
-                    }
-                }
-            }
-            answer += ch
+            val idx = (ch.indexOf(it) + index) % ch.size
+            answer += ch[idx]
         }
         
         return answer
