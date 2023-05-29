2f5a13658dd27cb0b58523ae5d4e5abe9932cdd8
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-05-29 18:41:34 +0900

    [unrated] Title: 명예의 전당 (1), Time: 9.42 ms, Memory: 62.9 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.kt
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.kt
@@ -1,24 +1,16 @@
+import java.util.*
+
 class Solution {
     fun solution(k: Int, score: IntArray): IntArray {
         var answer: IntArray = intArrayOf()
-        val HOF = ArrayList<Int>(k)
+        val pq = PriorityQueue<Int>()
         
         for (i in score.indices) {
-            if (HOF.size == k) {
-                HOF.sort()
-                if (HOF.first() < score[i]) {
-                    HOF.removeFirst()
-                    HOF.add(score[i])
-                    HOF.sort()
-                    answer += HOF.first()
-                } else {
-                    answer += HOF.first()
-                }
-            } else {
-                HOF.add(score[i])
-                HOF.sort()
-                answer += HOF.first()
+            pq.add(score[i])
+            if(pq.size > k) {
+                pq.poll()
             }
+            answer += pq.peek()
         }
         return answer
     }
