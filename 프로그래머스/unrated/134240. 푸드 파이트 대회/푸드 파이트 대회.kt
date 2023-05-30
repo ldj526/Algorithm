df0ce138fc84203a4c613fa6789673f3248218ca
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-05-30 18:29:47 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 19.05 ms, Memory: 61.1 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.kt
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.kt
@@ -2,21 +2,13 @@ class Solution {
     fun solution(food: IntArray): String {
         var answer: String = ""
         for (i in 1 until food.size) {
-            val cnt = food[i] / 2
-            repeat(cnt) {
+            repeat(food[i] / 2) {
                 answer += i
             }
         }
+        val rev = answer.reversed()
         answer += 0
-        for (i in food.size - 1 downTo 1) {
-            val cnt = food[i] / 2
-            repeat(cnt) {
-                answer += i
-            }
-        }
-        if (answer.length < 3) {
-            answer = ""
-        }
+        answer += rev
         return answer
     }
 }
