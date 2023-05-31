e96e7078854e8233c3689e7172ed20e126a91bfc
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-05-31 14:55:26 +0900

    [unrated] Title: 옹알이 (2), Time: 9.40 ms, Memory: 60.1 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.kt
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.kt
@@ -2,18 +2,9 @@ class Solution {
     fun solution(babbling: Array<String>): Int {
         var answer: Int = 0
         
-        for (i in 0 until babbling.size) {
-            if (babbling[i].contains("ayaaya") || babbling[i].contains("yeye")
-               || babbling[i].contains("woowoo") || babbling[i].contains("mama")) continue
-            
-            babbling[i] = babbling[i].replace("aya", " ")
-            babbling[i] = babbling[i].replace("ye", " ")
-            babbling[i] = babbling[i].replace("woo", " ")
-            babbling[i] = babbling[i].replace("ma", " ")
-            babbling[i] = babbling[i].replace(" ", "")
-            
-            if (babbling[i].isEmpty()) answer++
-        }
+        val pronounce = "aya|ye|woo|ma".toRegex()
+        val pronounce2 = "ayaaya|yeye|woowoo|mama".toRegex()
+        answer = babbling.map { it.replace(pronounce2, " ") }.map { it.replace(pronounce, "") }.filter { it == "" }.size
         return answer
     }
 }
