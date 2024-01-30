35b2bc0ca914aa18ba85ed49375b5547a69c210a
ldj526 <59276421+ldj526@users.noreply.github.com>
2024-01-30 15:16:30 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.02 ms, Memory: 60.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.kt
@@ -0,0 +1,8 @@
+import kotlin.math.sqrt
+
+class Solution {
+    fun solution(n: Long): Long {
+        val x = sqrt(n.toDouble()).toLong()
+        return if (x * x == n) (x + 1) * (x + 1) else -1
+    }
+}
