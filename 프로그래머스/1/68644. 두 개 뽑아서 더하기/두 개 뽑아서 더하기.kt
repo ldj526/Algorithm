5104dc17d34e5e45f1b445301d3c794423a11c60
ldj526 <59276421+ldj526@users.noreply.github.com>
2024-02-12 16:47:49 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 41.03 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(numbers: IntArray): IntArray {
+        var answer: IntArray = intArrayOf()
+        for (i in numbers.indices) {
+            for (j in i + 1 until numbers.size) {
+                answer += numbers[i] + numbers[j]
+            }
+        }
+        return answer.toSortedSet().toIntArray()
+    }
+}
