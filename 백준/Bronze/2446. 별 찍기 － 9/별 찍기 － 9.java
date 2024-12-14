6ee6c9f98d7f1805b0da19fd106bd5200c7b7d82
ldj526 <59276421+ldj526@users.noreply.github.com>
2024-12-14 23:01:53 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 116 ms, Memory: 14576 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println(" ".repeat(i - 1) + "*".repeat(2 * n - 2 * i + 1));
+        }
+        for (int i = n - 1; i >= 1; i--) {
+            System.out.println(" ".repeat(i - 1) + "*".repeat(2 * n - 2 * i + 1));
+
+        }
+    }
+}
