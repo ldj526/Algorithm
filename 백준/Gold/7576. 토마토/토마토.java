edd8f3134de707330a14775ad596bf9521639ee5
ldj526 <59276421+ldj526@users.noreply.github.com>
2024-12-25 23:23:38 +0900

    [Gold V] Title: 토마토, Time: 636 ms, Memory: 120784 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static int[][] box;
+
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {-1, 1, 0, 0};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+
+        box = new int[n][m];
+        Queue<int[]> queue = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                box[i][j] = Integer.parseInt(st.nextToken());
+                if (box[i][j] == 1) {
+                    queue.offer(new int[]{i, j});
+                }
+            }
+        }
+
+        System.out.print(bfs(queue));
+    }
+
+    private static int bfs(Queue<int[]> queue) {
+        int cnt = 0;
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+
+            for (int d = 0; d < 4; d++) {
+                int nx = current[0] + dx[d];
+                int ny = current[1] + dy[d];
+
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m || box[nx][ny] != 0) {
+                    continue;
+                }
+
+                box[nx][ny] = box[current[0]][current[1]] + 1;
+                queue.offer(new int[]{nx, ny});
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (box[i][j] == 0) {
+                    return -1;
+                }
+                cnt = Math.max(cnt, box[i][j]);
+            }
+        }
+
+        return cnt - 1;
+    }
+}
