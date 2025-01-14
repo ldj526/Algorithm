78f76b0a028e92b7b688b76ae7a8e83238dd1969
ldj526 <59276421+ldj526@users.noreply.github.com>
2025-01-14 20:34:23 +0900

    [Silver III] Title: 파도반 수열, Time: 100 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        long[] dp = new long[101];
+
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 1;
+
+        for (int i = 4; i <= 100; i++) {
+            dp[i] = dp[i - 2] + dp[i - 3];
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            int input = Integer.parseInt(br.readLine());
+            sb.append(dp[input]).append('\n');
+        }
+
+        System.out.print(sb);
+    }
+}
